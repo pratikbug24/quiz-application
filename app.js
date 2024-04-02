38ff2b7292d838cc94f481ffab6be95fec78b79c
pratikbug24 <PP7701264@GMAIL.COM>
2024-04-02 23:49:19 +0530

    initial comit
    add files

--- app.js
+++ app.js
@@ -79,19 +79,229 @@ var quiz = {
             },
             {
                 "id": 6,
-                "question": "What is the sizeof(char) in a 32-bit C compiler?",
+                "question": "Library functions are wrapped inside the_ _ _ _ _?",
                 "options": [
                 {
-                "a": "1 bit",
-                "b": "2 bits",
-                "c": "1 Byte",
-                "d": "2 Bytes"
+                "a": "Header File",
+                "b": "Library Functions",
+                "c": "Pointers",
+                "d": "Compiler"
                 }
                 ],
-                "answer": "1 Byte",
+                "answer": "Header Files",
                 "score": 0,
                 "status": ""
                 },
+                {
+                    "id": 7,
+                    "question": "_ _ _ _ is a value range of signed long int?",
+                    "options": [
+                    {
+                    "a": "-32768 to +32767 ",
+                    "b": "-2147483648 to +2147483647",
+                    "c": "-128 to +127",
+                    "d": "1 to 65536"
+                    }
+                    ],
+                    "answer": "-2147483648 to +2147483647",
+                    "score": 0,
+                    "status": ""
+                    },
+                    {
+                        "id": 8,
+                        "question": "Size of pointer variable is?",
+                        "options": [
+                        {
+                        "a": "4 byte",
+                        "b": "2 byte",
+                        "c": "1 byte",
+                        "d": "4 bit"
+                        }
+                        ],
+                        "answer": "2 bytes",
+                        "score": 0,
+                        "status": ""
+                        },
+                        {
+                            "id": 9,
+                            "question": "ASCII code for 'a' is?",
+                            "options": [
+                            {
+                            "a": "65",
+                            "b": "99",
+                            "c": "67",
+                            "d": "97"
+                            }
+                            ],
+                            "answer": "97",
+                            "score": 0,
+                            "status": ""
+                            },
+                            {
+                                "id": 10,
+                                "question": "Which one is not reserve keyword in C Language?",
+                                "options": [
+                                {
+                                "a": "auto",
+                                "b": "main",
+                                "c": "case",
+                                "d": "register"
+                                }
+                                ],
+                                "answer": "main",
+                                "score": 0,
+                                "status": ""
+                                },
+                                {
+                                    "id": 11,
+                                    "question": "Prototype of function means?",
+                                    "options": [
+                                    {
+                                    "a": "Name of Function",
+                                    "b": "Output of Function",
+                                    "c": "Input of Function",
+                                    "d": "Declaration of Function"
+                                    }
+                                    ],
+                                    "answer": "Input of Function",
+                                    "score": 0,
+                                    "status": ""
+                                    },
+                                    {
+                                        "id": 12,
+                                        "question": "The Default Parameter Passing Parameter is called as",
+                                        "options": [
+                                        {
+                                        "a": "Call by Value",
+                                        "b": "Call by Address",
+                                        "c": "Call by Reference",
+                                        "d": "Call by Name"
+                                        }
+                                        ],
+                                        "answer": "Call by Reference",
+                                        "score": 0,
+                                        "status": ""
+                                        },
+                                        {
+                                            "id": 13,
+                                            "question": "Structure is the_ _ _",
+                                            "options": [
+                                            {
+                                            "a": "Collection of only same data type",
+                                            "b": "Collection of only different data type",
+                                            "c": "Collection of same or different data type",
+                                            "d": "All the above"
+                                            }
+                                            ],
+                                            "answer": "Collection of same or different data type",
+                                            "score": 0,
+                                            "status": ""
+                                            },
+                                            {
+                                                "id": 14,
+                                                "question": "Which of the following accessesa variable in structure b?",
+                                                "options": [
+                                                {
+                                                "a": "b->var; ",
+                                                "b": "b.var;",
+                                                "c": "b-var;",
+                                                "d": "b>var;"
+                                                }
+                                                ],
+                                                "answer": "b.var;",
+                                                "score": 0,
+                                                "status": ""
+                                                },
+                                                {
+                                                    "id": 15,
+                                                    "question": "Which of the following is a properly defined struct ?",
+                                                    "options": [
+                                                    {
+                                                    "a": "struct {int a;}",
+                                                    "b": "struct a_struct {int a;}",
+                                                    "c": "struct a_struct int a;",
+                                                    "d": "struct a_struct {int a;};"
+                                                    }
+                                                    ],
+                                                    "answer": "struct a_struct {int a;};",
+                                                    "score": 0,
+                                                    "status": ""
+                                                    },
+                                                    {
+                                                        "id": 16,
+                                                        "question": "union is_ _ _ _?",
+                                                        "options": [
+                                                        {
+                                                        "a": "Inbuilt datatype",
+                                                        "b": "Inbuilt constant",
+                                                        "c": "User defined data type",
+                                                        "d": "All of the above"
+                                                        }
+                                                        ],
+                                                        "answer": "User defined data type",
+                                                        "score": 0,
+                                                        "status": ""
+                                                        },
+                                                        {
+                                                            "id": 17,
+                                                            "question": "What is the size of double data type?",
+                                                            "options": [
+                                                            {
+                                                            "a": "6 bytes",
+                                                            "b": "8 bytes",
+                                                            "c": "1 bytes",
+                                                            "d": "1 bit",
+                                                            }
+                                                            ],
+                                                            "answer": "8 bytes",
+                                                            "score": 0,
+                                                            "status": ""
+                                                            },
+                                                            {
+                                                                "id": 18,
+                                                                "question": "There are _ _ _ _ Keywords in C?",
+                                                                "options": [
+                                                                {
+                                                                "a": "32",
+                                                                "b": "34",
+                                                                "c": "64",
+                                                                "d": "48"
+                                                                }
+                                                                ],
+                                                                "answer": "32",
+                                                                "score": 0,
+                                                                "status": ""
+                                                                },
+                                                                {
+                                                                    "id": 19,
+                                                                    "question": "Let a=5 & b=7 then a*=b is ?",
+                                                                    "options": [
+                                                                    {
+                                                                    "a": "b=35",
+                                                                    "b": "a=35",
+                                                                    "c": "a=7",
+                                                                    "d": "b=12"
+                                                                    }
+                                                                    ],
+                                                                    "answer": "a=35",
+                                                                    "score": 0,
+                                                                    "status": ""
+                                                                    },
+                                                                    {
+                                                                        "id": 20,
+                                                                        "question": "Let a=5 & b=7 then a%=b is",
+                                                                        "options": [
+                                                                        {
+                                                                        "a": "b=5",
+                                                                        "b": "a=2",
+                                                                        "c": "b=1",
+                                                                        "d": "a=5"
+                                                                        }
+                                                                        ],
+                                                                        "answer": "a=5",
+                                                                        "score": 0,
+                                                                        "status": ""
+                                                                        },
                
 ]
 }
@@ -200,7 +410,7 @@ jsq.checkAnswer(selectedopt);
 jsq.changeQuestion(-1);
 });
 
-var totalTimeInSeconds = 20 * 60;
+var totalTimeInSeconds = 15 * 60;
 
 // Function to update the timer
 function updateTimer() {
